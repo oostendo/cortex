6fcf9e16370cf6c65d984ec07dd00eb28a292aea
Nate Oostendorp <nate@oostendorp.net>
2011-04-11 21:04:28 +0000

    centralize redis connection into model/__init__.py based on config params

--- cortex/lib/app_globals.py
+++ cortex/lib/app_globals.py
@@ -2,12 +2,14 @@
 
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
+from cortex.model import init_model
 
 class Globals(object):
     """Globals acts as a container for objects available throughout the
     life of the application
 
     """
+    
 
     def __init__(self, config):
         """One instance of Globals is created during application
@@ -16,3 +18,4 @@ class Globals(object):
 
         """
         self.cache = CacheManager(**parse_cache_config_options(config))
+        self.rd = init_model(config)
